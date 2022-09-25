10497d445cd80b3ea4e6742ca86448bd4901236f
Alex Borisov <108930927+sanych79@users.noreply.github.com>
2022-09-25 13:12:23 +0300

    init

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import os
 import shutil
 from tqdm import tqdm
+import json
 
 class VK:
 
@@ -21,10 +22,10 @@ class VK:
     
     
 
-   def get_photo_vk(self, count_f):
-        """Функция получения фотограций у пользователя self.id на его стекне album_id' = "wall" количесто фото 'count_f' """
+   def get_photo_vk(self, count_f, alboum = 'wall'):
+        """Функция получения фотограций у пользователя self.id на его стене album_id' = "wall" количесто фото 'count_f' """
         url = self.url + 'photos.get'
-        params = {'owner_id': -self.id, 'album_id': "wall", 'count': count_f, 'extended':1}
+        params = {'owner_id': -self.id, 'album_id': alboum, 'count': count_f, 'extended':1}
         response = requests.get(url, params={**self.params, **params}).json()
         return response
 
@@ -73,6 +74,7 @@ if __name__ == "__main__":
     def  get_maxsize_photo(serch_data={}):
         """Функция поиска максимального размера фото. Выход размер, url, количество лайков"""
         size = 0
+        size_type = 0
         m_url = 0
         m_like = 0
         count = 1
@@ -81,8 +83,9 @@ if __name__ == "__main__":
                 size = y['width']*y['height']
                 m_url = y['url']
                 m_like = x['likes']['count']
+                size_type = y['type']
             count = 0
-        return size, m_url, m_like
+        return size, m_url, m_like, size_type
 
     def read_token(file_name):
         """Функция чтения токена из файлаfile_name."""
@@ -91,19 +94,31 @@ if __name__ == "__main__":
                 res =  line.strip()
         return res
 
+    def Serch_req(mane):
+        result = 0
+        with open("requiremеnts.txt",'r', encoding='utf8') as file:
+            """Функция поиска параметра mane в файле requiremеnts.txt"""
+            res = file.readlines()
+            for x in res:
+                str = x.strip()
+                if str.find(mane) == 0:
+                    result = str[len(mane)+1:]
+        return result
+
+    """чтение токенов из файлов"""
     vk_token = read_token('token_vk.txt')
     yd_token = read_token('token_yadisk.txt')
 
-    "user_id - id пользоватля в VK"
-    user_id = 1
-    "count_file - количество скачиваемых фото VK"
-    count_file = 3
+    """чтение параметров программы из requiremеnts.txt"""
+    user_id = int(Serch_req('user_id'))
+    count_file = int(Serch_req('count_file'))
+    dir_mane = str(Serch_req('dir_mane'))
+    album_i = str(Serch_req('album_id'))
+
     vk = VK(vk_token, user_id)
     uploader = YaUploader(yd_token)
 
-
-    # создаем временную папку для фото
-    dir_mane = 'BackUp_Photo'
+    """ создаем временную папку для фото"""
     path1 = os.getcwd()
     path2 = path1 +f'\{dir_mane}'
     access_rights = 0o755
@@ -116,26 +131,32 @@ if __name__ == "__main__":
        print ("Успешно создана директория %s " % path2)
 
     os.chdir(path2)
-
+    main_result = []
 
     Count_step_statusbar = int(100/count_file)
     with tqdm(total=100) as pbar:
-        for x in vk.get_photo_vk(count_f= count_file)['response']['items']:
+        for x in vk.get_photo_vk(count_f= count_file, alboum=album_i)['response']['items']:
             like_count = x['likes']['count']
 
-            size, max_url, max_like = get_maxsize_photo(x['sizes'])
-
+            size, max_url, max_like, size_type = get_maxsize_photo(x['sizes'])
             url = max_url
             r = requests.get(url)
             result = uploader.md(dir_mane)
             with open(f'{max_like}.jpg', 'wb') as f:
                 f.write(r.content)  
                 result = uploader.upload(f'{max_like}.jpg', f'BackUp_Photo/{max_like}.jpg')
+                main_result.append({"file_name": f'{max_like}.jpg', "size": size_type})
                 pbar.update(Count_step_statusbar)
 
     os.chdir(path1)
 
-    shutil.rmtree(path2) # удаляем временную папку
+    """ даляем временную папку """
+    shutil.rmtree(path2)
+
+    """ запись результата в файл result.json """
+    with open('result.json', 'w') as f:
+        json.dump(main_result, f)
+
 
 
 
