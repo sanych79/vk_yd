0cc771c71dfd02e9ecf50d1398d48deff9722bfc
Alex Borisov <108930927+sanych79@users.noreply.github.com>
2022-10-02 11:34:47 +0300

    modif

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import requests
-import os
-import shutil
 from tqdm import tqdm
 import json
 
@@ -41,13 +39,13 @@ class YaUploader:
         h = self.get_headers()
         param = {"path": disk_file_path, "overwrite": True}
         response = requests.get(up_url, headers=h, params=param)
-        #print(response.json())
         return response.json()
 
     def upload(self, file_path_from: str, file_path_to: str):
         """Метод загружает файлы по списку file_path на яндекс диск"""
         href = self._get_upload_link(disk_file_path=file_path_to).get("href", "")
-        response = requests.put(href, data=open(file_path_from, 'rb'))
+        #response = requests.put(href, data=open(file_path_from, 'rb'))
+        response = requests.put(href, data=file_path_from)
         response.raise_for_status()
         #print(response)
 
@@ -72,11 +70,11 @@ def  get_maxsize_photo(serch_data):
     m_url = 0
     m_like = 0
     count = 1
-    for y in x['sizes']:
+    for y in serch_data['sizes']:
         if size < y['width']*y['height'] or count == 1:
             size = y['width']*y['height']
             m_url = y['url']
-            m_like = x['likes']['count']
+            m_like = serch_data['likes']['count']
             size_type = y['type']
         count = 0
     return size, m_url, m_like, size_type
@@ -108,56 +106,63 @@ def read_parametrs():
     global vk_token
     global yd_token
     dir_mane = str(Serch_req('dir_mane'))
+    dir_mane = str(Serch_req('dir_mane'))
     album_i = str(Serch_req('album_id'))
     vk_token = str(Serch_req('vk_token'))
     yd_token = str(Serch_req('yd_token'))
 
-
-
-if __name__ == "__main__":
-
-    read_parametrs()
-    user_id = int(input('Введи ID пользователя'))
-    count_file = int(input('Введите количество выгружаемых файлов'))
-
-    vk = VK(vk_token, user_id)
-    uploader = YaUploader(yd_token)
-
-    """ создаем временную папку для фото"""
-    path1 = os.getcwd()
-    path2 = path1 +f'\{dir_mane}'
-    access_rights = 0o755
-    
     try:
-       os.makedirs(path2)
-    except OSError:
-       print ("Создать директорию %s не удалось" % path2)
+        user_id = int(input('Введи ID пользователя>'))
+    except ValueError:
+        print('Введено не корректное значения')
+        user_id = int(Serch_req('user_id'))
+        print(f'Присвоенно значение по умолчанию :{user_id}')
     else:
-       print ("Успешно создана директория %s " % path2)
+        if user_id < 1:
+            print('Введено не корректное значения')
+            user_id = int(Serch_req('user_id'))
+            print(f'Присвоенно значение по умолчанию :{user_id}')
 
-    os.chdir(path2)
-    main_result = []
+    try:
+        count_file = int(input('Введите количество выгружаемых файлов>'))
+    except ValueError:
+        print('Введено не корректное значения')
+        count_file = int(Serch_req('count_file'))
+        print(f'Присвоенно значение по умолчанию :{count_file}')
+    else:
+        if count_file < 1:
+            print('Введено не корректное значения')
+            count_file = int(Serch_req('count_file'))
+            print(f'Присвоенно значение по умолчанию :{count_file}')
 
+def main_function(main_res):
+    """Основная функция которая получает данные из ВК и записывает на ЯДиск"""
     Count_step_statusbar = int(100/count_file)
+    last_max_like = 0
     with tqdm(total=100) as pbar:
-        for x in vk.get_photo_vk(count_f= count_file, alboum=album_i)['response']['items']:
+        for x in vk.get_photo_vk(count_f=count_file, alboum=album_i)['response']['items']:
             like_count = x['likes']['count']
-
-            size, max_url, max_like, size_type = get_maxsize_photo(x['sizes'])
+            size, max_url, max_like, size_type = get_maxsize_photo(x)
+            if max_like == last_max_like:
+                file_name = f'{max_like}_{x["date"]}'
+            else:
+                file_name = max_like
+            last_max_like = max_like
             url = max_url
-            r = requests.get(url)
             result = uploader.md(dir_mane)
-            with open(f'{max_like}.jpg', 'wb') as f:
-                f.write(r.content)  
-                result = uploader.upload(f'{max_like}.jpg', f'BackUp_Photo/{max_like}.jpg')
-                main_result.append({"file_name": f'{max_like}.jpg', "size": size_type})
-                pbar.update(Count_step_statusbar)
-
-    os.chdir(path1)
-
-    """ удаляем временную папку """
-    shutil.rmtree(path2)
+            result = uploader.upload(url, f'BackUp_Photo/{file_name}.jpg')
+            main_res.append({"file_name": f'{file_name}.jpg', "size": size_type})
+            pbar.update(Count_step_statusbar)
 
     """ запись результата в файл result.json """
     with open('result.json', 'w') as f:
         json.dump(main_result, f)
+
+if __name__ == "__main__":
+
+    read_parametrs()
+    vk = VK(vk_token, user_id)
+    uploader = YaUploader(yd_token)
+    main_result = []
+    main_function(main_result)
+    print("Конец работы программы")
